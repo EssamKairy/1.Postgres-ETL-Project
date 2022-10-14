56b1188f1e7784d374116bf6263869830a9aba8c
Essam Khairy Dahi <kairyesam@gmail.com>
2022-10-14 11:36:55 -0700

    ETL Project

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Data Engineer FWD 
- Data Engineer Track
+## Description
+---
+This repo provides the ETL pipeline, to populate the sparkifydb database. 
+In this project, I'll apply what I've learned on data modeling with Postgres and build an ETL pipeline using Python. To complete the project, I will need to define fact and dimension tables for a star schema for a particular analytic focus, and write an ETL pipeline that transfers data from files in two local directories into these tables in Postgres using Python and SQL.
+* The purpose of this database is to enable Sparkify to answer business questions it may have of its users, the types of songs they listen to and the artists of those songs using the data that it has in logs and files. The database provides a consistent and reliable source to store this data.
+
+* This source of data will be useful in helping Sparkify reach some of its analytical goals, for example, finding out songs that have highest popularity or times of the day which is high in traffic.
+
+## Database Design and ETL Pipeline
+---
+* For the schema design, the STAR schema is used as it simplifies queries and provides fast aggregations of data.
+## Schema for Song Play Analysis
+# Fact Table
+* songplays - records in log data associated with song plays i.e. records with page NextSong songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+# Dimension Tables
+* 1 - users - users in the app: user_id, first_name, last_name, gender, level
+
+* 2 - songs - songs in music database: song_id, title, artist_id, year, duration
+
+* 3 - artists - artists in music database: artist_id, name, location, latitude, longitude
+
+* 4 - time - timestamps of records in songplays broken down into specific units: start_time, hour, day, week, month, year, weekda
+
+![Schema](schema.PNG)
+
+* For the ETL pipeline, Python is used as it contains libraries such as pandas, that simplifies data manipulation. It also allows connection to Postgres Database.
+
+* There are 2 types of data involved, song and log data. For song data, it contains information about songs and artists, which we extract from and load into users and artists dimension tables
+
+* Log data gives the information of each user session. From log data, we extract and load into time, users dimension tables and songplays fact table.
+
+## Running the ETL Pipeline
+---
+* First, run create_tables.py to create the data tables using the schema design specified. If tables were created previously, they will be dropped and recreated.
+
+* Next, run etl.py to populate the data tables created.
+
+## etl.py File Contains:
+* process_song_file Function This function can be used to read the file in the filepath (data/song_data)
+  to get the song and artist info and used to populate the songs and artists dim tables.
+* process_log_file Function This function can be used to read the file in the filepath (data/log_data)
+  to get the user and time info and used to populate the users and time dim tables.
+* process_data Function get all files matching JSON extension from directory
+
+## Create_tables.py File Contains:
+* create_database Function: Creates and connects to the sparkifydb and Returns the connection and cursor to sparkifydb
+* drop_tables Function: Drops each table using the queries in `drop_table_queries` list.
+* create_tables Function:  Creates each table using the queries in `create_table_queries` list.
+
+## sql_queries.py File Contains:
+* list of postgresql Queries that Create or Drop all tables and insert data into the table
---
+++ etl.py
@@ -0,0 +1,130 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+import json
+import datetime as dt
+
+def process_song_file(cur, filepath):
+    """
+      Description: 
+        process_song_file Function This function can be used to read the file in the filepath (data/song_data)
+        to get the song and artist info and used to populate the songs and artists dim tables.
+      Arguments:
+          cur: the cursor object. 
+          filepath: songs data files path. 
+
+      Returns:
+          None
+    """
+    # open song file
+    df = df = pd.read_json(filepath, lines=True)
+
+    # insert song record
+    song_data  = list(df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0])
+
+    cur.execute(song_table_insert, song_data)
+    
+    # insert artist record
+    artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0]) 
+    cur.execute(artist_table_insert, artist_data)
+
+
+def process_log_file(cur, filepath):
+    """
+      Description: This function can be used to read the file in the filepath (data/log_data)
+      to get the user and time info and used to populate the users and time dim tables.
+
+      Arguments:
+          cur: the cursor object. 
+          filepath: log data file path. 
+
+      Returns:
+          None
+    """
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+
+    # filter by NextSong action
+    df = df[df['page'] == 'NextSong']
+
+    # convert timestamp column to datetime
+    t = pd.to_datetime(df['ts'])
+    
+    # insert time data records
+    time_data = [(dt.datetime.fromtimestamp(int(tt.value/1000)), tt.hour, tt.day, tt.week, tt.month, tt.year, tt.weekday()) for tt in t]
+    column_labels = ('timestamp', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame(data=time_data, columns=column_labels)
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
+
+    # load user table
+    user_df = user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
+
+    # insert songplay records
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = (dt.datetime.fromtimestamp(int(row['ts']/1000)), row['userId'], row['level'], songid, artistid, row['sessionId'],
+                     row['location'], row['userAgent'])
+        cur.execute(songplay_table_insert, songplay_data)
+
+
+def process_data(cur, conn, filepath, func):
+    
+    """
+      Description: get all files matching JSON extension from directory
+      Arguments:
+          cur: the cursor object. 
+          conn: is the database Connection
+          filepath: data file path. 
+          func: is taking another function as an argument, and returning a new function EX: process_song_file.
+
+      Returns:
+          list of filepath
+    """
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
